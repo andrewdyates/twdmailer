35585b73d5683c50735ec2c46b8da9e1ebc62789
Andrew D. Yates <andrew.yates@hhmds.com>
2010-11-05 14:37:10 -0400

    working status flags for lead list

--- user.py
+++ user.py
@@ -3,6 +3,7 @@
 # Copyright © 2010 Andrew D. Yates
 import logging
 import os
+import datetime
 
 from google.appengine.api import users
 from google.appengine.ext import webapp
@@ -16,18 +17,15 @@ import models
 class LeadListing(base.BasePage):
   
   def get(self):
-
-    Q_SIZE = 100
-    self.template = "lead_listing.html"
-
     # this should be middleware in base.BasePage
     if not self.account:
       self.render_page()
       return
 
-    cursor = self.request.get("cursor", None)
+    Q_SIZE = 100
+    self.template = "lead_listing.html"
+    cursor = self.request.get("cursor", "")
     pg = self.request.get("pg", 1)
-    next_pg = "%s" % (int(pg) + 1)
     
     q = models.Lead.all().filter("account =", self.account).order("-date_created")
     if cursor:
@@ -37,9 +35,11 @@ class LeadListing(base.BasePage):
     if len(leads) < Q_SIZE:
       self.ctx['next_link'] = None
     else:
-      next_cursor = q.cursor()
+      self.ctx['this_cursor'] = cursor
+      self.ctx['next_cursor'] = q.cursor()
+      self.ctx['next_pg'] = "%s" % (int(pg) + 1)
       self.ctx['next_link'] = "%s?cursor=%s&pg=%s" % \
-        (self.request.path, next_cursor, next_pg)
+        (self.request.path, self.ctx['next_cursor'], self.ctx['next_pg'])
 
     if cursor:
       self.ctx['top_link'] = self.request.path
@@ -51,6 +51,35 @@ class LeadListing(base.BasePage):
     self.ctx['set_size'] = str(Q_SIZE)
 
     self.render_page()
+
+  def post(self):
+    # this should be middleware in base.BasePage
+    if not self.account:
+      self.render_page()
+      return
+
+    act = self.request.get("action")
+    key = self.request.get("key")
+    page = self.request.get("page")
+    this_cursor = self.request.get("this_cursor")
+    lead = models.Lead.get(key)
+    if not lead:
+      logging.warning("No lead for key %s." % key)
+      return 
+
+    if act == 'mail hardcopy':
+      lead.date_hard_copy_mailed = datetime.datetime.now()
+      logging.info("Lead %s flagged for hard copy mailed." % lead.email)
+      lead.put()
+    elif act == 'close lead':
+      lead.date_closed = datetime.datetime.now()
+      logging.info("Lead %s flagged for closed." % lead.email)
+      lead.put()
+    else:
+      logging.warning("Invalid action %s." % act)
+      return
+
+    self.redirect("%s?pg=%s&cursor=%s" % (self.request.path, page, this_cursor))
 
 
 class EmailAttachment(base.BasePage):
